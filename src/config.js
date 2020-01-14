839784c30131723dcc5303df73c61c4b90a6e1ff
Eli Reiners <goelyukim@gmail.com>
2020-01-14 11:46:58 -0500

    Update2

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$n9Ml92tfQ5fKv2wQ6ZvHhOOnMF7bT/I.2V9Zi6zE8A1wzoaldDeoW',
 }
