1605a98a6159e49e6064f08b2f429a1c3d3652a1
Eli Reiners <goelyukim@gmail.com>
2020-07-05 12:41:21 -0400

    Add update method

--- src/BookmarksContext.js
+++ src/BookmarksContext.js
@@ -4,6 +4,7 @@ const BookmarksContext = React.createContext({
     bookmarks: [],
     addBookmark: () => {},
     deleteBookmark: () => {},
+    updateArticle: () => {},
   })
   
   export default BookmarksContext
